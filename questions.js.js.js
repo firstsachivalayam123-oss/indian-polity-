135b189ebd513944fa038f636c50965d5a486432
firstsachivalayam123-oss <firstsachivalayam123@gmail.com>
2026-09-02 11:44:21 +0530

    Add files via upload

---
+++ questions.js.js.js
@@ -0,0 +1,14 @@
+const questions = [
+  {
+    "id": 1,
+    "question": "Indus Valley Civilization lo main port city edi?",
+    "options": ["Harappa", "Mohenjo-daro", "Lothal", "Kalibangan"],
+    "answer": 2
+  },
+  {
+    "id": 2,
+    "question": "Maurya Empire ni sthapinchindi evaru?",
+    "options": ["Ashoka", "Chandragupta Maurya", "Bindusara", "Samudragupta"],
+    "answer": 1
+  }
+];
